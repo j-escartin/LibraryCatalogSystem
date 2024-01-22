e485de5896ab2c847df6fa2b5db3aefc55e0ae0f
James Vincent Escartin <javines2403@gmail.com>
2024-01-23 05:37:06 +0800

    added Library Class

---
+++ Library.py
@@ -0,0 +1,24 @@
+class Library:
+  def __init__(self):
+    self.books = [] # Initialize Books
+
+  # Add Books
+  def add_book(self, book):
+    print(f"You have successfully added the book entitle {book.title} to the library catalog")
+    self.books.append(book)
+
+  # Search By Title
+  def search_by_title(self, title):
+    matching_books = [book for book in self.books if book.title.lower() == title.lower()]
+    return matching_books
+  
+  # Search by Author
+  def search_by_author(self, author):
+    matching_books = [book for book in self.books if book.author.lower() == author.lower()]
+    return matching_books
+  
+  # Display all Catalogs
+  def display_catalog(self):
+    print("Displaying all cards in the catalog")
+    for book in self.books:
+      book.display_info()
