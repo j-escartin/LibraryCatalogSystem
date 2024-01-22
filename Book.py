d97ca11a8985dd363a694414602f91998c32eaf6
James Vincent Escartin <javines2403@gmail.com>
2024-01-22 21:27:22 +0800

    added Book classs

---
+++ Book.py
@@ -0,0 +1,22 @@
+class Book: # Declare the Class Book
+
+  def __init__(self, title, author, availability = True): # Initialize the init constructor
+    self.title = title
+    self.author = author
+    self.availability = availability
+  
+  def display_info(self):
+    print(f"Title: {self.title}")
+    print(f"Author: {self.author}")
+    print("Availability: Available" if self.availability else "Availability: Not Available")
+    print()
+
+  def check_out(self):
+    if self.availability:
+      print(f"You have checkout the book entitled: {self.title}")
+      self.availability = False
+    else:
+      print(f"Sorry the book entitled: {self.title} is not available")
+
+  def check_in(self):
+    print(f"Thank you for returning the book entitled: {self.title}")
